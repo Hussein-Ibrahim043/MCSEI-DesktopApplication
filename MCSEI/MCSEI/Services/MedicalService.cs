0761f79bd82249159bfae7e8575cb152b8a7fd3c
Hussein Ibrahim <167726476+Hussein-Ibrahim043@users.noreply.github.com>
2025-06-23 22:21:10 +0300

    Update MedicalService.cs

--- MCSEI/MCSEI/Services/MedicalService.cs
+++ MCSEI/MCSEI/Services/MedicalService.cs
@@ -16,7 +16,7 @@ namespace Final_Project_SHA_V1._2.Services
     public class MedicalService : IMedicalService
     {
         private static readonly HttpClient client = new HttpClient();
-        private static readonly string URL = "https://medical-website-three-delta.vercel.app";
+        private static readonly string URL = "https://mcsei-production.up.railway.app";
 
         // Static constructor to initialize base address for HttpClient.
         static MedicalService()
